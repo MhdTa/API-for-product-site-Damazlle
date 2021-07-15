c6489976b3beb0aa9fbc6f2b047a9b09dca932f0
Mohamed Taha <49076857+MhdTa@users.noreply.github.com>
2021-07-15 09:17:22 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\productsController;
+use App\Http\Controllers\BidOfferController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,23 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//log out Route
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
+//log in Route
+Route::post('/login', [LoginController::class, 'store']);
+//sign in Route
+Route::post('/register', [RegisterController::class, 'store']);
+//products Route
+Route::get('/products', [productsController::class, 'index'])->name('products');
+Route::get('/products/{productId}', [productsController::class, 'show'])->name('users.product');
+Route::post('/products', [productsController::class, 'store']);
+Route::delete('/products/{productId}', [productsController::class, 'destroy'])->name('products.destroy');
+Route::PUT('/products/{productId}', [productsController::class, 'update'])->name('products.update');
+Route::post('/products/{productId}', [productsController::class, 'sell'])->name('products.sell');
+Route::get('/search', [productsController::class, 'search'])->name('products.search');
+//Bids Offer Route
+Route::post('/bidOffer/{userId}/{productId}', [BidOfferController::class, 'store'])->name('bidOffer.add');
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
